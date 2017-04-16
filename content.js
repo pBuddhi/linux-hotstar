d38f9d0457e5fb4dbf7c2fcc9fdabe40d7e8c24f
buddhi_prakash <buddhi9417@gmail.com>
2017-04-16 22:14:15 +0530

    making it neat

--- content.js
+++ content.js
@@ -1,14 +1,5 @@
 
-
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-// alert(firstHref);
-console.log(firstHref);
 var xhr = new XMLHttpRequest();
-
-
-
-
-
 // var pathname = window.location.pathname; // Returns path only
 var url      = window.location.href;
 
@@ -20,36 +11,16 @@ xhr.open("GET", "http://getcdn.hotstar.com/AVS/besc?action=GetCDN&asJson=Y&chann
 xhr.send();
 var result = xhr.responseText;
 console.log(result);
-// console.log(Object.prototype.toString.call(result));
-// result = result.split(",");
-// var src = result[result.length-3];
-// src = src.substring(7,src.length-1);
-// console.log(src);
+
 var obj = JSON.parse(result);
 // obj = JSON.parse(obj);
 var s = obj.resultObj.src;
-console.log(obj.resultObj.src);
-// chrome.tabs.create({"url": s});
-var arr = s.split(",180");
-if(arr.length>1) s = arr[0]+arr[1];
-arr = s.split(",400");
-if(arr.length>1) s = arr[0]+arr[1];
-arr = s.split(",800");
-if(arr.length>1) s = arr[0]+arr[1];
-arr = s.split(",1300");
-if(arr.length>1) s = arr[0]+arr[1];
-console.log(s);
-console.log("yahan asdfagda");
+
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = s;
-
-      console.log(firstHref);
-
-      // This line is new!
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": s});
     }
   }
 );
