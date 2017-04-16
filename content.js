21835db51f3f5930113ac48addd42cca9794d137
buddhi_prakash <buddhi9417@gmail.com>
2017-04-16 12:40:12 +0530

    first commit

---
+++ content.js
@@ -0,0 +1,53 @@
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+// alert(firstHref);
+console.log(firstHref);
+var xhr = new XMLHttpRequest();
+
+
+
+
+
+// var pathname = window.location.pathname; // Returns path only
+var url      = window.location.href;
+
+// console.log(pathname);
+console.log(url);
+var lastPart = url.split("/").pop();
+console.log(lastPart);
+xhr.open("GET", "http://getcdn.hotstar.com/AVS/besc?action=GetCDN&asJson=Y&channel=TABLET&id="+lastPart+"&type=VOD", false);
+xhr.send();
+var result = xhr.responseText;
+console.log(result);
+// console.log(Object.prototype.toString.call(result));
+// result = result.split(",");
+// var src = result[result.length-3];
+// src = src.substring(7,src.length-1);
+// console.log(src);
+var obj = JSON.parse(result);
+// obj = JSON.parse(obj);
+var s = obj.resultObj.src;
+console.log(obj.resultObj.src);
+// chrome.tabs.create({"url": s});
+var arr = s.split(",180");
+s = arr[0]+arr[1];
+arr = s.split(",400");
+s = arr[0]+arr[1];
+arr = s.split(",800");
+s = arr[0]+arr[1];
+arr = s.split(",1300");
+s = arr[0]+arr[1];
+
+console.log("yahan asdfagda");
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = s;
+
+      console.log(firstHref);
+
+      // This line is new!
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+    }
+  }
+);
